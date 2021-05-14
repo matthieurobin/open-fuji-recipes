6a2bf3e89d9203b36047b31796462894daf5f809
Matthieu Robin <mrobin@ideolys.com>
2021-05-14 20:15:49 +0200

    build to docs folder instead of dist (for github pages)

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build : {
+    outDir : 'docs'
+  },
   plugins: [
     svelte(),
     VitePWA({
