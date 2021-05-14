170b692e6930890191fc6f2641d43acbb8d2f70c
Matthieu Robin <mrobin@ideolys.com>
2021-05-14 20:27:13 +0200

    fix build

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base  : '',
   build : {
-    outDir : 'docs'
+    outDir : 'docs',
+    target : 'es2015',
   },
   plugins: [
     svelte(),
