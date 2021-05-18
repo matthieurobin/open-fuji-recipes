9d823c28d0778197f16601ad3c95358e4dc5ee3d
matthieurobin <robin10mat@gmail.com>
2021-05-18 20:24:37 +0200

    add style for parameter name

--- src/module/recipes/_recipe-param.svelte
+++ src/module/recipes/_recipe-param.svelte
@@ -1,5 +1,5 @@
 <div>
-  <span class="dark:text-gray-200">
+  <span class="dark:text-gray-200 tracking-wide">
     <slot name="name"></slot>
   </span>
   <span class="font-semibold ml-1.5 text-black dark:text-white">
